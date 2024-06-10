370baee79c19f477eab775e9f3ccdb0cd962ff1b
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-06-09 22:03:06 -0600

    𝑳𝒚𝑩𝒐𝒕

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -22,7 +22,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `   
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 ❄
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 🪻
  ┃➤
  ┃➤    𝗛ola, ${taguser}
  ┃➤
@@ -32,14 +32,14 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ 𝐅𝐞𝐜𝐡𝐚 : ${date}
  ╰━━━━━━━━━━━━━
   
- ╭━      ❄ 𝑰𝒏𝒇𝒐 𝑳𝒚𝑩𝒐𝒕 ❄ 
+ ╭━      🪻 𝑰𝒏𝒇𝒐 𝑳𝒚𝑩𝒐𝒕 🪻 
  ┃➤ ${usedPrefix}listagrupos
  ┃➤ ${usedPrefix}estado
  ┃➤ ${usedPrefix}creador
  ┃➤ ${usedPrefix}velocidad
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑱𝒖𝒆𝒈𝒐𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑱𝒖𝒆𝒈𝒐𝒔 🪻 
  ┃➤ ${usedPrefix}mates | matemáticas | math
  ┃➤ ${usedPrefix}lanzar *cara* | *cruz*
  ┃➤ ${usedPrefix}ppt *piedra : papel : tijera*
@@ -85,7 +85,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}suerte
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑨𝒖𝒔𝒕𝒆𝒔 𝑫𝒆𝒍 𝑮𝒓𝒖𝒑𝒐  ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑨𝒖𝒔𝒕𝒆𝒔 𝑫𝒆𝒍 𝑮𝒓𝒖𝒑𝒐  🪻 
  ┃➤ ${usedPrefix}enable *o* disable *bienvenida | welcome*
  ┃➤ ${usedPrefix}enable *o* disable *avisos | detect*
  ┃➤ ${usedPrefix}enable *o* disable *autonivel | autolevelup*
@@ -115,7 +115,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}enable *o* disable *sologrupos | gconly*  
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑫𝒆𝒔𝒄𝒂𝒓𝒈𝒂𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑫𝒆𝒔𝒄𝒂𝒓𝒈𝒂𝒔 🪻 
  ┃➤ ${usedPrefix}imagen | image *texto*
  ┃➤ ${usedPrefix}pinterest | dlpinterest *texto*
  ┃➤ ${usedPrefix}wallpaper|wp *texto*
@@ -145,7 +145,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}historia
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑪𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝑫𝒆𝒍 𝑮𝒓𝒖𝒑𝒐 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑪𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝑫𝒆𝒍 𝑮𝒓𝒖𝒑𝒐 🪻 
  ┃➤ ${usedPrefix}add *numero*
  ┃➤ ${usedPrefix}sacar | ban | kick  *@tag*
  ┃➤ ${usedPrefix}grupo *abrir o cerrar*
@@ -180,7 +180,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}disable
   ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑪𝒐𝒏𝒗𝒆𝒓𝒕𝒊𝒅𝒐𝒓𝒆𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑪𝒐𝒏𝒗𝒆𝒓𝒕𝒊𝒅𝒐𝒓𝒆𝒔 🪻 
  ┃➤ ${usedPrefix}toimg | img | jpg *sticker*
  ┃➤ ${usedPrefix}toanime | jadianime *foto*
  ┃➤ ${usedPrefix}tomp3 | mp3 *video o nota de voz*
@@ -191,12 +191,12 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}tts es *texto*
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑳𝒐𝒈𝒐𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑳𝒐𝒈𝒐𝒔 🪻 
  ┃➤ ${usedPrefix}logos *efecto texto*
  ┃➤ ${usedPrefix}menulogos2
 ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑳𝒐𝒈𝒐𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑳𝒐𝒈𝒐𝒔 🪻 
  *┃➤* _${usedPrefix}simpcard *@tag*_
  *┃➤* _${usedPrefix}hornycard *@tag*_
  *┃➤* _${usedPrefix}lolice *@tag*_
@@ -207,7 +207,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   ╰━━━━━━━━━━━━━
 
  
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑨𝒏𝒊𝒎𝒆 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑨𝒏𝒊𝒎𝒆 🪻 
  ┃➤ ${usedPrefix}chica
  ┃➤ ${usedPrefix}chico
  ┃➤ ${usedPrefix}cristianoronaldo
@@ -260,7 +260,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}cosplay
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑬𝒇𝒆𝒄𝒕𝒐𝒔 ❄
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑬𝒇𝒆𝒄𝒕𝒐𝒔 🪻
  ┃➤ ${usedPrefix}bass
  ┃➤ ${usedPrefix}blown
  ┃➤ ${usedPrefix}deep
@@ -275,7 +275,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}tupai
  ╰━━━━━━━━━━━━━
   	
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑩𝒖𝒔𝒒𝒖𝒆𝒅𝒂𝒔 ❄
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑩𝒖𝒔𝒒𝒖𝒆𝒅𝒂𝒔 🪻
  ┃➤ ${usedPrefix}animeinfo *texto*
  ┃➤ ${usedPrefix}anuel
  ┃➤ ${usedPrefix}conejo
@@ -290,7 +290,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}wiki | wikipedia *texto*
  ╰━━━━━━━━━━━━━
 
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑯𝒆𝒓𝒓𝒂𝒎𝒊𝒆𝒏𝒕𝒂𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑯𝒆𝒓𝒓𝒂𝒎𝒊𝒆𝒏𝒕𝒂𝒔 🪻 
  ┃➤ ${usedPrefix}afk *motivo*
  ┃➤ _${usedPrefix}acortar *url*
  ┃➤ ${usedPrefix}calc *operacion math*
@@ -307,11 +307,11 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ╰━━━━━━━━━━━━━
 
 
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑻𝒐𝒑𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑻𝒐𝒑𝒔 🪻 
  ┃➤ ${usedPrefix}top
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 🪻 
  ┃➤ ${usedPrefix}sticker | s *imagen o video*
  ┃➤ ${usedPrefix}sticker | s *url de tipo jpg*
  ┃➤ ${usedPrefix}emojimix *😺+😆*
@@ -331,12 +331,12 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}cs *:* cs2
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 2 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 2 🪻 
  ┃➤ ${usedPrefix}wm *packname|author*
  ┃➤ ${usedPrefix}wm *texto1|texto2*
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 3 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑺𝒕𝒊𝒄𝒌𝒆𝒓𝒔 3 🪻 
  ┃➤ ${usedPrefix}palmaditas | pat *@tag*
  ┃➤ ${usedPrefix}bofetada | slap *@tag*
  ┃➤ ${usedPrefix}golpear *@tag*
@@ -344,7 +344,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
  ┃➤ ${usedPrefix}alimentar | food *@tag*
  ╰━━━━━━━━━━━━━
   
- ╭━     ❄ 𝑳𝒚𝑩𝒐𝒕 𝑶𝒘𝒏𝒆𝒓𝒔 ❄ 
+ ╭━     🪻 𝑳𝒚𝑩𝒐𝒕 𝑶𝒘𝒏𝒆𝒓𝒔 🪻 
  ┃➤ ${usedPrefix}join *enlace*
  ┃➤ ${usedPrefix}unete *enlace*
  ┃➤ ${usedPrefix}dardiamantes *cantidad*
