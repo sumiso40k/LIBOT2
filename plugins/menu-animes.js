55f59650212abc63cff8bfa5ba7b808a94d81812
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-06-09 19:53:02 -0600

    𝑳𝒚𝑩𝒐𝒕

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -19,7 +19,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `
-   𝐒𝐇𝐀𝐃𝐎𝐖.𝐁𝐎𝐓 ⭐
+   𝑳𝒚𝑩𝒐𝒕
 
  *𝗛ola, ${taguser}*
 
@@ -66,7 +66,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ⭐_${usedPrefix}sakura_
 ⭐_${usedPrefix}cosplay_
 
-𝐒𝐇𝐀𝐃𝐎𝐖.𝐁𝐎𝐓 ⭐`.trim();
+𝑳𝒚𝑩𝒐𝒕`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
