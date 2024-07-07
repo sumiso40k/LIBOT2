51449ff024226e9d3c8433ecf26a25c643bfd03b
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-07-06 21:07:17 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -1425,7 +1425,7 @@ export async function participantsUpdate({id, participants, action}) {
       if (chat.welcome && !chat?.isBanned) {
         const groupMetadata = await m.conn.groupMetadata(id) || (conn.chats[id] || {}).metadata;
         for (const user of participants) {
-          let pp = './src/avaly.png';
+          let pp = './src/avalybot.png';
           try {
             pp = await m.conn.profilePictureUrl(user, 'image');
           } catch (e) {
