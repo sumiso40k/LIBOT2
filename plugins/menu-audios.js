4a3686470768789da1440b04a88aadb19c9e393c
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-07-06 21:09:43 -0600

    Update menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   try {
     const pp = imagen4;
     // let vn = './media/menu.mp3'
-    const img = './menly.png';
+    const img = './lymenu.png';
     const d = new Date(new Date + 3600000);
     const locale = 'es';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
